38985d78373b7f2d51af9985a89acf22688ad77d
조성원 <tjddnjsjo@kakao.com>
2024-03-14 09:12:04 +0900

    feat: uppercase 속성 추가

--- app/simple-substitution-cipher/components/substitute.tsx
+++ app/simple-substitution-cipher/components/substitute.tsx
@@ -50,7 +50,7 @@ export function Substitute({
         <h3 className="text-2xl font-semibold tracking-tight">
           Decrypted Plaintext
         </h3>
-        <p className="mt-2 text-lg">{plaintext}</p>
+        <p className="mt-2 text-lg uppercase">{plaintext}</p>
       </AccordionContent>
     </AccordionItem>
   )
